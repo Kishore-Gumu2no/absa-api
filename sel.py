1aacb4118d34dcbd0951052d19ebc1b83fda5b02
ENDrxc <1dt23ic013@dsatm.edu.in>
2025-11-06 19:33:36 +0530

    Add project imports and load_dotenv

---
+++ sel.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import os
+import psycopg2
+import time
+load_dotenv()
